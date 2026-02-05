178a49f4cdeef58bec117aff21ac2c2c9f7088c7
Vineet Padole <vineetpadole137@gmail.com>
2026-02-05 23:07:07 +0530

    Add f-string example in f-string.py

---
+++ f-string.py
@@ -0,0 +1,9 @@
+ #f-strings
+# this is use for writing variables in strings  
+
+syntax : f"string {variable}"
+
+name = "John"
+age = 30
+height = 5.9           
+print(f"My name is {name}, I am {age} years old and my height is {height} feet.")

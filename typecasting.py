5531f07b822d4a665cc4f7e3158f611b129d2119
Vineet Padole <vineetpadole137@gmail.com>
2026-02-06 21:10:43 +0530

    Document typecasting process in typecasting.py
    Added comments explaining typecasting in Python.

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+# # Typecasting
+# #the process of converting a variable from one data type to another
+# # str(), int(), float(), bool()
+# # int() -> str()
+# # float() -> str()
+# # bool() -> str()
+# # int() -> float()
+# # float() -> int()
+
+# #code : 
+# bool(False) -> int() #O/P : 0
+# bool(True) -> int() #O/P : 1
+# bool(False) -> float() #O/P : 0.0 
+# bool(True) -> float() #O/P : 1.0
+# int(0) -> bool() #O/P : False
+# int(1) -> bool() #O/P : True        
+# float(0.0) -> bool() #O/P : False
+# float(1.0) -> bool() #O/P : True

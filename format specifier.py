43e70c84fb877350a623c9cea92cb62f3ac7d06b
Vineet Padole <vineetpadole137@gmail.com>
2026-02-09 00:21:31 +0530

    Fix formatting of price3_ variable

---
+++ format specifier.py
@@ -0,0 +1,11 @@
+#format specifiers
+#format a value based on what flags are inserted
+Syntax: {value:flags}
+
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+
+price1_ = 3000.14159
+price2_ = -9870.65
+price3_ = 1200.34

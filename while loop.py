dac2b16a8a082b7414db085db274839582c7ee2e
Vineet Padole <vineetpadole137@gmail.com>
2026-02-10 22:20:06 +0530

    Remove while loop for food input

--- while loop.py
+++ while loop.py
@@ -12,10 +12,3 @@ print(f"Hello {name}")
 #     Enter your name: Vineet
 #     Hello Vineet
 
-food = input("Enter a food you like (q to quit): ")
-while not food == "q": #When food is not q it will continue to run the loop but if the user puts in q then we will exit the loop  
-    print(f"You like {food}")
-    food = input("Enter another food you like (q to quit): ")
-print ("bye")
-
-# Similarly we can also use 'or' and 'and' operators
